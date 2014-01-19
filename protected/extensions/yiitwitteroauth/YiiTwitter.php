c4c671b53245091569d3dcd7d661a677430e9206
Yiorgos <giorgos.tavatzopoulos@peopleperhour.com>
2014-01-19 10:42:51 +0200

    Update YiiTwitter.php

--- protected/extensions/yiitwitteroauth/YiiTwitter.php
+++ protected/extensions/yiitwitteroauth/YiiTwitter.php
@@ -4,19 +4,21 @@ class YiiTwitter extends CApplicationComponent
 {
 
 	//The Twitter Apps key, set in config.
-	public $consumer_key = '';
+	public $consumerKey = '';
 	
 	//The Twitter Apps secret key, set in config.	
-	public $consumer_secret = '';
+	public $consumerSecret = '';
 	
 	//The call back url for twitter
 	public $callback = '';	
 	
 	//Can be set in config to not load OAuth.php
-	public $load_oauth = true;	
+	public $loadOauth = true;	
 	
 	//Have we loaded our dependencies
 	private static $registeredScripts = false;
+	
+	private $_instance = null;
 
 	public function init() {
 		$this->registerScripts();
@@ -34,8 +36,11 @@ class YiiTwitter extends CApplicationComponent
 	/**
 	* Use this one for when we need to authicate oursevles with twitter
 	*/
-	public function getTwitter() {
-		return new TwitterOAuth($this->consumer_key,$this->consumer_secret);			
+	public function getTwitter($refresh=false) {
+		if ($this->_instance === null || $refresh) {
+			$this->_instance = new TwitterOAuth($this->consumerKey,$this->consumerSecret);
+		}
+		return 	$this->_instance;
 	}
 
 	/**
@@ -43,7 +48,7 @@ class YiiTwitter extends CApplicationComponent
 	*	(you must save these in order for them to be usefull
 	*/
 	public function getTwitterTokened($token,$secret) {
-		return new TwitterOAuth($this->consumer_key,$this->consumer_secret,$token,$secret);	
+		return new TwitterOAuth($this->consumerKey,$this->consumerSecret,$token,$secret);	
 	}
 		
     /**
@@ -52,7 +57,7 @@ class YiiTwitter extends CApplicationComponent
     public function registerScripts() {
     	if (self::$registeredScripts) return;
     	self::$registeredScripts = true;
-		if($this->load_oauth)
+		if($this->loadOauth)
 			require dirname(__FILE__).'/OAuth.php';
 		require dirname(__FILE__).'/twitteroauth.php';
 	}	
